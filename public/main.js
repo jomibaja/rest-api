5e5410ede8272bd8b5504a09de4501cc0975dd28
Jose Miguel <jose.baraona@gmail.com>
2020-10-23 16:30:46 -0300

    creamos mascotas, para esto guardamos los datos del formulario, los pasamos a un json y los enviamos a la base de datos, de respuesta recibimos el id de la mascota creada y junto con el set de datos orignales, los agregamos a la lista que se esta mostrando en la pantalla

--- public/main.js
+++ public/main.js
@@ -2,8 +2,9 @@ const darDeAlta = id => {
     console.log(id)
 }
 
+const url = "https://us-central1-rest-api-6fc7a.cloudfunctions.net/api/pets"
+
 const fetchPets = async () => {
-    const url = "https://us-central1-rest-api-6fc7a.cloudfunctions.net/api/pets"
     const response = await fetch(url)
     const json = await response.json()
 
@@ -19,7 +20,35 @@ const tableTemplate = ({ _id, nombre, tipo, descripción }) => `
     </tr>
 `
 
+const handleSubmit = async e => {
+    e.preventDefault()
+    const { nombre, tipo, descripción } = e.target
+    const data = {
+        nombre: nombre.value,
+        tipo: tipo.value,
+        descripción: descripción.value,
+    }
+    nombre.value = ''
+    tipo.value = ''
+    descripción.value = ''
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+    })
+    const json = await response.json()
+    const template = tableTemplate({
+        ...data,
+        _id: json,
+    })
+
+    const tabla = document.getElementById('tabla')
+    tabla.insertAdjacentHTML('beforeend', template)
+}
+
 window.onload = async () => {
+    const petForm = document.getElementById('pet-form')
+    petForm.onsubmit = handleSubmit
     const pets = await fetchPets()
     const template = pets.reduce((acc, el) => 
     acc + tableTemplate(el), '')
